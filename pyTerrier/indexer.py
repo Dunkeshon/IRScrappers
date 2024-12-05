55ded8a958d74bba9ef6130c0c4e3684801c7aed
Polad <bakhip@usi.ch>
2024-12-06 00:45:47 +0100

    indexer takes preprocessed docs from mongo and generates ikea index

--- pyTerrier/indexer.py
+++ pyTerrier/indexer.py
@@ -7,28 +7,13 @@ if not pt.java.started():
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["IR_Ikea"]
-reviews_collection = db["reviews"]
-articles_collection = db["articles"]
+processed_collection = db["processed_documents"]
 
-def get_documents(collection):
-    docs = collection.find()
-    documents = []
-    for doc in docs:
-        docno = str(doc["_id"])
-        title = doc.get("articleTitle", "No title available")
-        text = doc.get("articleText", "No text available")
-        print(f"Document ID: {docno}, Title: {title}, Text: {text}")
-        documents.append({"docno": docno, "title": title, "text": text})
-    return pd.DataFrame(documents)
-
-reviews_df = get_documents(reviews_collection)
-articles_df = get_documents(articles_collection)
-
-all_docs_df = pd.concat([reviews_df, articles_df])
+processed_df = pd.DataFrame(list(processed_collection.find()))
 
 index_path = "./ikea_index"
 indexer = pt.IterDictIndexer(index_path, meta={'docno': 24, 'title': 50, 'text': 1000})
 
-indexer.index(all_docs_df[['docno', 'text', 'title']].to_dict(orient="records"))
+indexer.index(processed_df[['docno', 'text', 'title']].to_dict(orient="records"))
 
 print(f"Indexing complete! Index stored at: {index_path}")
