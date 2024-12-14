6b3f977743db010fcf417930e67a979d1fce80a9
Polad <bakhip@usi.ch>
2024-12-14 22:07:06 +0100

    added search to app

--- ikea-search/src/App.tsx
+++ ikea-search/src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Search from "./components/Search.tsx";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
