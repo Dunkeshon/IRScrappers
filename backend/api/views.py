82b81367a1e021d3dc7aacffea294d120972d359
Polad <bakhip@usi.ch>
2024-12-14 19:48:15 +0100

    django views done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import pyterrier as pt
+import os
+from django.conf import settings
 
-# Create your views here.
+if not pt.java.started():
+    pt.java.init()
+
+index_path = os.path.join(settings.BASE_DIR, "..", "pyTerrier", "ikea_index")
+index = pt.IndexFactory.of(index_path)
+
+retriever = pt.BatchRetrieve(index, num_results=10, metadata=["docno", "title", "text"])
+
+@api_view(['GET'])
+def search_view(request):
+    query = request.GET.get('query', '')  # Example: /api/search?query=ikea
+    if not query:
+        return Response({"error": "Query parameter is required"}, status=400)
+    
+    results = retriever.search(query)
+    
+    data = results[['docno', 'title', 'text', 'score']].to_dict(orient='records')
+    
+    return Response(data)
